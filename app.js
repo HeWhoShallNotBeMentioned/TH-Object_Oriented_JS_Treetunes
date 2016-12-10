8e81d4e4ea132a648f53848e193010119ea564bc
Chris Underwood <cunderwoodmn@gmail.com>
2016-12-09 20:47:48 -0800

    updated playlist, app.js, and song

---
+++ app.js
@@ -0,0 +1,11 @@
+var playlist = new Playlist();
+
+var subDivisions = new Song("Sub-Divisions", "Rush", "5:15");
+var tomSawyer = new Song("Tom Sawyer", "Rush", "4:30");
+var limeLight = new Song("Limelight", "Rush", "5:01");
+var goodbye = new Song("Goodbye", "The Sundays", "4:24");
+
+playlist.add(subDivisions);
+playlist.add(tomSawyer);
+playlist.add(limeLight);
+playlist.add(goodbye);
